1847409ba6b4cff32c65fdfbabc24ba233414635
Patrik Tichy <patrick.tichy@gmail.com>
2025-02-04 11:00:36 +0100

    some changes

--- content.js
+++ content.js
@@ -2,18 +2,22 @@ if (window.location.href.startsWith('https://rbixm.qualtrics.com/')) {
     console.log('start 1.0.0');
 
     let bkCode = '18440';
-    /* let datum = '2024-09-05'; */
     let datum = '05/09/2024';
     let ora = '12';
     let perc = '22';
     let napszak = '1';
 
+    bkCode = null;
+    datum = null;
+    ora = null;
+    perc = null;
+    napszak = null;
+
     let intervall;
     const buttonStop = document.createElement('BUTTON');
-    buttonStop.innerText = 'Start';
-    buttonStop.style.position = 'fixed';
-    buttonStop.style.bottom = '0';
-    buttonStop.style.left = '0';
+    buttonStop.innerText = 'Start2';
+    buttonStop.id = 'play-button';
+
     buttonStop.addEventListener('click', () => {
         if (intervall) {
             clearInterval(intervall);
@@ -26,86 +30,31 @@ if (window.location.href.startsWith('https://rbixm.qualtrics.com/')) {
     });
     document.body.appendChild(buttonStop);
 
-    let vanButton = false;
-    let resolveActive = false;
-    let page = 0;
-
     function iterate() {
         console.log('Iterate');
 
-        if (page == 0) {
-            let input = document.querySelector('#QR\\~QID4');
-            if (input) {
-                input.value = bkCode;
-                clickNext();
-                page++;
-            }
-        }
+        const labelSingle = document.getElementsByClassName('SingleAnswer')?.[0];
+        const labelMulti = document.getElementsByClassName('MultipleAnswer')?.[0];
+        const tds = document.getElementsByClassName('c4');
+        const textarea = document.getElementsByClassName('InputText')?.[0];
 
-        const latogatasSpan = document.querySelector('#choice1QID8 > span > label > span');
-        if (latogatasSpan) {
-            page = 1;
-        } else if (page === 1) {
-            page++;
-        }
+        if (labelSingle || labelMulti || tds?.length || textarea) {
+            if (labelSingle && labelSingle?.id?.startsWith('QID')) {
+                labelSingle.click();
+            } else if (labelMulti && labelMulti.id.startsWith('QID')) {
+                labelMulti.click();
+            } else if (tds.length) {
+                for (let i = 0; i < tds.length; i++) {
+                    const input = tds[i].querySelectorAll('input[type="radio"]')?.[0];
 
-        if (page === 1) {
-            const dateInput = document.querySelector('#QR\\~QID118\\~2');
-            const oraSelect = document.querySelector('#QR\\~QID8\\#1\\~1');
-            const percSelect = document.querySelector('#QR\\~QID8\\#2\\~1');
-            const napszakSelect = document.querySelector('#QR\\~QID8\\#3\\~1');
-
-            if (dateInput && oraSelect && percSelect && napszakSelect) {
-                dateInput.value = datum;
-                oraSelect.value = ora;
-                percSelect.value = perc;
-                napszakSelect.value = napszak;
-            }
-
-            const nextBtn = document.querySelector('#NextButton');
-            nextBtn?.addEventListener('click', () => {
-                vanButton = false;
-            });
-
-            if (!vanButton) {
-                let asd = document.createElement('BUTTON');
-                asd.innerText = 'BKResolve!';
-                asd.id = 'PreviousButton';
-                asd.addEventListener('click', () => {
-                    nextBtn?.click();
-                    resolveActive = true;
-                });
-
-                nextBtn?.parentNode?.insertBefore(asd, nextBtn);
-
-                vanButton = true;
-
-                asd.click();
-            }
-        } else if (page > 1 && resolveActive) {
-            const labelSingle = document.getElementsByClassName('SingleAnswer')?.[0];
-            const labelMulti = document.getElementsByClassName('MultipleAnswer')?.[0];
-            const tds = document.getElementsByClassName('c4');
-            const textarea = document.getElementsByClassName('InputText')?.[0];
-
-            if (labelSingle || labelMulti || tds?.length || textarea) {
-                if (labelSingle && labelSingle?.id?.startsWith('QID')) {
-                    labelSingle.click();
-                } else if (labelMulti && labelMulti.id.startsWith('QID')) {
-                    labelMulti.click();
-                } else if (tds.length) {
-                    for (let i = 0; i < tds.length; i++) {
-                        const input = tds[i].querySelectorAll('input[type="radio"]')?.[0];
-
-                        if (input) {
-                            input.click();
-                        }
+                    if (input) {
+                        input.click();
                     }
-                } else if (textarea) {
-                    textarea.value = 'Hacked by soza';
                 }
-                clickNext();
+            } else if (textarea) {
+                textarea.value = 'Hacked by soza';
             }
+            clickNext();
         }
     }
 
--- open-urls.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-timeout /t 5
-start https://another-example.com
-start https://third-example.com
